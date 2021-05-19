5487cffdbcd4e1389f3dc9d678487c9f464f3d94
mcs754 <mcs754@gmail.com>
2021-05-19 10:26:30 -0300

    Projeto de caculadora de câmbio em arquivo .php único finalizado

--- calculadora.php
+++ calculadora.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculadora de Câmbio</title>
+    <title>Conversor de Moedas</title>
     <style type="text/css">
         body {
             font-family: 'Segoe UI', sans-serif;
@@ -16,6 +16,7 @@
             color: #bfbfff;
             background-color: #111173;
             text-align: center;
+            margin-top: 10%;
         }
 
         select {
@@ -90,82 +91,122 @@
 
 <body>
     <?php
-    //criando array com os valore numéricos e inserindo os valores numa list de variáveis
-    $a = array(5.2755, 6.4134, 7.4513, 1, 0.0483, 0.8191);
+    //criando array com os valores numéricos das moedas e inserindo os valores numa list de variáveis
+    $a = array(5.2587986, 6.417299, 7.4600978, 1, 0.04824, 0.8185);
     list($dlr, $eur, $lbr, $brl, $ine, $ren) = $a;
-    //transformando os valores numéricos em números com quatro casas decimais após o ponto, conforme Banco Central do Brasil, mas preservando variável original para operações matemáticas
-    $dolar = number_format($dlr, 4, '.', '');
-    $euro = number_format($eur, 4, '.', '');
-    $libra = number_format($lbr, 4, '.', '');
-    $real = number_format($brl, 4, '.', '');
-    $iene = number_format($ine, 4, '.', '');
-    $renminbi = number_format($ren, 4, '.', '');
-    //sobrepondo vírgula no lugar de ponto, mas preservando variável original para operações matemáticas
-    $dolar = str_replace('.', ',', $dlr);
-    $euro = str_replace('.', ',', $eur);
-    $libra = str_replace('.', ',', $lbr);
-    $real = str_replace('.', ',', $brl);
-    $iene = str_replace('.', ',', $ine);
-    $renminbi = str_replace('.', ',', $ren);
+    //transformando os valores numéricos em números com quatro casas decimais após o ponto, mas preservando variável original para operações matemáticas
+    $dlr2 = number_format($dlr, 4, '.', '');
+    $eur2 = number_format($eur, 4, '.', '');
+    $lbr2 = number_format($lbr, 4, '.', '');
+    $brl2 = number_format($brl, 4, '.', '');
+    $ine2 = number_format($ine, 4, '.', '');
+    $ren2 = number_format($ren, 4, '.', '');
+    //sobrepondo vírgula no lugar de ponto
+    $dolar = str_replace('.', ',', $dlr2);
+    $euro = str_replace('.', ',', $eur2);
+    $libra = str_replace('.', ',', $lbr2);
+    $real = str_replace('.', ',', $brl2);
+    $iene = str_replace('.', ',', $ine2);
+    $renminbi = str_replace('.', ',', $ren2);
+    //iniciando cabeçalho básico da calculadora de câmbio apresentando informações úteis
     ?>
-    <?php //cabeçalho básico da calculadora de câmbio
-    ?>
-    <h2>Casa de câmbio Muito Dinheiro</h2>
-    <h3>Cotação das principais moedas do mundo hoje</h3>
+    <h1>Casa de câmbio Muito Dinheiro</h1>
+    <h2>Cotação das principais moedas do mundo hoje</h2>
     <h3>Dólar Americano (USD) R$ <?php echo $dolar ?> • Euro (EUR) R$ <?php echo $euro ?> • Iene (JPY) R$ <?php echo $iene ?> • Libra Esterlina (GBP) R$ <?php echo $libra ?> • Renminbi (CNY) R$ <?php echo $renminbi ?></h3>
-    <?php //formulário para possibilitar o usuário informar os parâmetros para o câmbio
+    <?php //implementando formulário para o usuário informar os parâmetros para o câmbio
     ?>
-    <div>
-        <form action="calculadora.php" method="post">
-            <label>Moeda de origem</label>
-            <select name="origem" required>
-                <option value=<?php echo $dlr ?>>Dólar (USD)</option>
-                <option value=<?php echo $eur ?>>Euro (EUR)</option>
-                <option value=<?php echo $ine ?>>Iene (JPY)</option>
-                <option value=<?php echo $lbr ?>>Libra (GBP)</option>
-                <option selected value=<?php echo $brl ?>>Real (BRL)</option>
-                <option value=<?php echo $ren ?>>Renminbi (CNY)</option>
-            </select>
-            <label>Moeda de destino</label>
-            <select name="destino" required>
-                <option selected value=<?php echo $dlr ?>>Dólar (USD)</option>
-                <option value=<?php echo $eur ?>>Euro (EUR)</option>
-                <option value=<?php echo $ine ?>>Iene (JPY)</option>
-                <option value=<?php echo $lbr ?>>Libra (GBP)</option>
-                <option value=<?php echo $brl ?>>Real (BRL)</option>
-                <option value=<?php echo $ren ?>>Renminbi (CNY)</option>
-            </select>
-            <label>Valor:</label>
-            <input name="valor" type="number" placeholder="0,0000" step="any" required>
-            <button id="botao" type="submit">=<i class="fas fa-exchange-alt"></i></button>
-            <?php
-            //capturando as informações do formulário que foi preenchido pelo usuário
-            if ($_POST) {
-                $origem = $_POST['origem'];
-                $destino = $_POST['destino'];
-                $valor = $_POST['valor'];
-                //realizando as operações baseadas nos requisitos
-                $convert = $valor * $origem; //convertendo a moeda de origem em real
-                $calc = $convert / $destino; //convertendo o resultado da conversão anterior em moeda de destino
-                $desc = (10 / 100) * $calc; //aplicando o desconto de 10%
-                $saldo = $calc - $desc; //mensurando resultado final
-                //tranformando o resultado em números com duas casas decimais e sobrepondo a vírgula
-                $valor = number_format($valor, 4, '.', '');
-                $valor = str_replace('.', ',', $valor);
-                $calc = number_format($calc, 4, '.', '');
-                $calc = str_replace('.', ',', $calc);
-                $desc = number_format($desc, 4, '.', '');
-                $desc = str_replace('.', ',', $desc);
-                $saldo = number_format($saldo, 4, '.', '');
-                $saldo = str_replace('.', ',', $saldo);
-                //apresentando os resultados das operações em tela
-                echo ('<p>Conversão de ' . $valor . ' para ' . $calc . '</p>');
-                echo ('<p>Desconto de ' . $desc . ' equivalente a 10% de taxa de serviço</p>');
-                echo ('<h3>Saldo do cliente ' . $saldo . '</h3>');
+    <form action="calculadora.php" method="post">
+        <label>Moeda de origem</label>
+        <select name="origem" required>
+            <option value=<?php echo $dlr ?>>Dólar (USD)</option>
+            <option value=<?php echo $eur ?>>Euro (EUR)</option>
+            <option value=<?php echo $ine ?>>Iene (JPY)</option>
+            <option value=<?php echo $lbr ?>>Libra (GBP)</option>
+            <option selected value=<?php echo $brl ?>>Real (BRL)</option>
+            <option value=<?php echo $ren ?>>Renminbi (CNY)</option>
+        </select>
+        <label>Moeda de destino</label>
+        <select name="destino" required>
+            <option selected value=<?php echo $dlr ?>>Dólar (USD)</option>
+            <option value=<?php echo $eur ?>>Euro (EUR)</option>
+            <option value=<?php echo $ine ?>>Iene (JPY)</option>
+            <option value=<?php echo $lbr ?>>Libra (GBP)</option>
+            <option value=<?php echo $brl ?>>Real (BRL)</option>
+            <option value=<?php echo $ren ?>>Renminbi (CNY)</option>
+        </select>
+        <label>Valor:</label>
+        <input name="valor" type="number" placeholder="0,0000" step="any" required>
+        <button id="botao" type="submit">=<i class="fas fa-exchange-alt"></i></button>
+        <?php
+        //capturando as informações do formulário que foi preenchido pelo usuário
+        if ($_POST) {
+            $origem = $_POST['origem'];
+            $destino = $_POST['destino'];
+            $valor = $_POST['valor'];
+            //realizando as operações baseadas nos requisitos
+            $convert = $valor * $origem; //convertendo a moeda de origem em real
+            $calc = $convert / $destino; //convertendo o resultado da conversão anterior em moeda de destino
+            $desc = (10 / 100) * $calc; //aplicando o desconto de 10%
+            $saldo = $calc - $desc; //mensurando resultado final
+            //tranformando o resultado em números com quatro casas decimais e sobrepondo a vírgula
+            $valor = number_format($valor, 4, '.', '');
+            $valor = str_replace('.', ',', $valor);
+            $calc = number_format($calc, 4, '.', '');
+            $calc = str_replace('.', ',', $calc);
+            $desc = number_format($desc, 4, '.', '');
+            $desc = str_replace('.', ',', $desc);
+            $saldo = number_format($saldo, 4, '.', '');
+            $saldo = str_replace('.', ',', $saldo);
+            //capturando informação da moeda para identificá-la e apresentá-la ao usuário. Foi utilizado switch case para evitar processamento desnecessário de informações
+            $cifra_origem = ' ';
+            switch ($cifra_origem) {
+                case $origem == $dlr;
+                    $cifra_origem = 'USD';
+                    break;
+                case $origem == $eur;
+                    $cifra_origem = 'EUR';
+                    break;
+                case $origem == $ine;
+                    $cifra_origem = 'JPY';
+                    break;
+                case $origem == $lbr;
+                    $cifra_origem = 'GBP';
+                    break;
+                case $origem == $brl;
+                    $cifra_origem = 'BRL';
+                    break;
+                case $origem == $ren;
+                    $cifra_origem = 'CNY';
+                    break;
             }
-            ?>
-        </form>
-    </div>
+            $cifra_destino = ' ';
+            switch ($cifra_destino) {
+                case $destino == $dlr;
+                    $cifra_destino = 'USD';
+                    break;
+                case $destino == $eur;
+                    $cifra_destino = 'EUR';
+                    break;
+                case $destino == $ine;
+                    $cifra_destino = 'JPY';
+                    break;
+                case $destino == $lbr;
+                    $ccifra_destino = 'GBP';
+                    break;
+                case $destino == $brl;
+                    $cifra_destino = 'BRL';
+                    break;
+                case $destino == $ren;
+                    $cifra_destino = 'CNY';
+                    break;
+            }
+            //apresentando os resultados das operações em tela
+            echo ('<p>Conversão de ' . $cifra_origem . ' ' . $valor . ' para ' . $cifra_destino . ' ' . $calc . '</p>');
+            echo ('<p>Desconto de ' . $cifra_destino . ' ' . $desc . ' equivalente a 10% de taxa de serviço</p>');
+            echo ('<h3>Saldo do cliente: ' . $cifra_destino . ' ' . $saldo . '</h3>');
+        }
+        ?>
+    </form>
 </body>
 
 </html>
